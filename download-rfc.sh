5cad2017a1cd98f138d6ccee273fdb96e9e6be2f
zaneway <zaneway1108@gmail.com>
2025-02-06 09:38:56 +0800

    update download script

--- download-rfc.sh
+++ download-rfc.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 for i in {1..10000}; do 
     if [[ ! -e "rfc$i.txt.pdf" ]]; then
-      #  echo "rfc$i.txt.pdf文件存在"
-    #else
         echo "rfc$i.txt.pdf 文件不存在，开始尝试下载"
-        wget -ncv https://www.rfc-editor.org/pdfrfc/rfc$i.txt.pdf 2>/dev/null ; 
+        # 部分文件官网不存在,忽略下载错误信息
+        wget -nc https://www.rfc-editor.org/pdfrfc/rfc$i.txt.pdf 2>/dev/null ;
     fi
 done
