ac02a2ec8e62bd50ea1dd96c7f3e84c10524b3cf
zaneway <zaneway1108@gmail.com>
2026-01-28 08:50:06 +0800

    完善脚本

--- download-rfc.sh
+++ download-rfc.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 for i in {1..10000}; do 
-    if [[ ! -e "pdf/rfc$i.txt.pdf" ]]; then
+    if [[ ! -e "pdf/rfc$i.txt.pdf" && ! -e "pdf/rfc$i.pdf" ]]; then
         echo "pdf/rfc$i.txt.pdf 文件不存在，开始尝试下载"
+
         # 部分文件官网不存在,忽略下载错误信息
-        wget -nc https://www.rfc-editor.org/pdfrfc/rfc$i.txt.pdf 2>/dev/null ;
+        wget -nc  https://www.rfc-editor.org/pdfrfc/rfc$i.txt.pdf 2>/dev/null ;
+
+     if [[ ! -e "rfc$i.txt.pdf" ]]; then
+        wget -nc https://www.rfc-editor.org/rfc/rfc$i.pdf 2>/dev/null ;
+         
+     fi
+
     fi
 done
 mv *.pdf pdf/
